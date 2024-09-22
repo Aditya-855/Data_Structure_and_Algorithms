78b19ba8c220da76ab08fa0a3c63067555dd2c64
Aditya-855 <139979027+Aditya-855@users.noreply.github.com>
2024-09-22 12:04:08 +0530

    Daily Coding Practice

---
+++ src/Interview.java
@@ -0,0 +1,12 @@
+//public class Interview {
+//    public static void main(String[] args) {
+//
+//       try{
+//           int y=10/0;
+//       } catch (Exception e) {
+//           e.printStackTrace(e);
+//       }catch (ArithmeticException){
+//           System.out.println("error");
+//       }
+//    }
+//}
