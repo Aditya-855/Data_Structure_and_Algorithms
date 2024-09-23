85c14c7e0b5fcac857813099a7a0f6fc366e0529
Aditya-855 <139979027+Aditya-855@users.noreply.github.com>
2024-09-23 21:56:49 +0530

    Daily Coding Practice

--- src/Leetcode/Dynamic_Programming/Jump_Game.java
+++ src/Leetcode/Dynamic_Programming/Jump_Game.java
@@ -19,4 +19,31 @@ public class Jump_Game{
         }
         return false;
     }
+//    int maxIndex = 0;
+//
+//    // Iterate through each
+//    // index of the array
+//        for (int i = 0; i < nums.length; i++) {
+//        // If the current index is greater
+//        // than the maximum reachable index
+//        // it means we cannot move forward
+//        // and should return false
+//        if (i > maxIndex) {
+//            return false;
+//        }
+//
+//        // Update the maximum index
+//        // that can be reached by comparing
+//        // the current maxIndex with the sum of
+//        // the current index and the
+//        // maximum jump from that index
+//        maxIndex = Math.max(maxIndex, i + nums[i]);
+//    }
+//
+//    // If we complete the loop,
+//    // it means we can reach the
+//    // last index
+//        return true;
+//}
+
 }
