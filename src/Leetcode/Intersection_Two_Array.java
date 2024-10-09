79acf746bb96fd5e49c7bcfdf57721d307422fd2
Aditya-855 <139979027+Aditya-855@users.noreply.github.com>
2024-10-09 22:58:51 +0530

    Daily Coding Practice

---
+++ src/Leetcode/Intersection_Two_Array.java
@@ -0,0 +1,40 @@
+package Leetcode;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class Intersection_Two_Array {
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 2, 1};
+        int[] nums2 = {2, 2};
+
+        int[] result = intersection(nums1, nums2);
+        // Print the result
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set = new HashSet<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        for (int i = 0; i < nums1.length; i++) {
+            set.add(nums1[i]);
+        }
+
+        for (int i = 0; i < nums2.length; i++) {
+            if (set.contains(nums2[i])) {
+                ans.add(nums2[i]);
+                set.remove(nums2[i]);
+            }
+        }
+
+        // Convert ArrayList to array
+        int[] arr = new int[ans.size()];
+        for (int i = 0; i < ans.size(); i++) {
+            arr[i] = ans.get(i);
+        }
+        return arr;
+    }
+}
