883a5ada708a9a54c3ec08bb42780281174475fb
Aditya-855 <139979027+Aditya-855@users.noreply.github.com>
2024-11-13 22:52:22 +0530

    Daily Coding Practice

---
+++ src/Graphs/Number_Of_Provinces.java
@@ -0,0 +1,39 @@
+package Graphs;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class Number_Of_Provinces {
+    private static void dfs(int node, ArrayList<ArrayList<Integer>> adjLs,int vis[]){
+        vis[node]=1;
+        for(Integer it:adjLs.get(node)){
+            if(vis[it]==0){
+                dfs(it,adjLs,vis);
+            }
+        }
+    }
+    static int numProvinces(ArrayList<ArrayList<Integer>> adj,int V){
+        ArrayList<ArrayList<Integer>> adjLs=new ArrayList<ArrayList<Integer>>();
+        for (int i=0;i<V;i++){
+            adjLs.add(new ArrayList<Integer>());
+        }
+        //to change adjacency matrix to list
+        for(int i=0;i<V;i++){
+            for (int j=0;j<V;j++){
+                if(adj.get(i).get(j)==1 && i!=j){
+                    adjLs.get(i).add(j);
+                    adjLs.get(j).add(i);
+                }
+            }
+        }
+        int vis[] =new int[V];
+        int cnt=0;
+        for(int i=0;i<V;i++){
+            if(vis[i]==0){
+                cnt++;
+                dfs(i,adjLs,vis);
+            }
+        }
+        return cnt;
+    }
+}
