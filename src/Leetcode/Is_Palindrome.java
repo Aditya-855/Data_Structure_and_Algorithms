c0f3063b867a58a98ca6dfbc3e830a90ba776ce8
Aditya-855 <139979027+Aditya-855@users.noreply.github.com>
2024-12-05 22:19:44 +0530

    Leetcode Question Is_Palindrome

---
+++ src/Leetcode/Is_Palindrome.java
@@ -0,0 +1,22 @@
+package Leetcode;
+
+public class Is_Palindrome {
+    public static void main(String[] args) {
+            int x = 121;
+        System.out.println(isPalindrome(x));
+    }
+    public static boolean isPalindrome(int x){
+        int temp=x;
+        //int digit=0;
+        int rev=0;
+        while(x>0){
+            int digit=x%10;
+            rev=rev*10+digit;
+            x=x/10;
+        }
+        if(rev==temp){
+            return true;
+        }
+        return false;
+    }
+}
