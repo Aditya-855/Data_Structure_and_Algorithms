2193afbda9a3692ea7a256e88aba59eb7653b97a
Aditya-855 <139979027+Aditya-855@users.noreply.github.com>
2025-01-22 22:19:13 +0530

    Leetcode Question Merge Two Sorted List

--- src/Leetcode/Merge_Two_Sorted_List.java
+++ src/Leetcode/Merge_Two_Sorted_List.java
@@ -2,7 +2,7 @@ package Leetcode;
 
 class ListNode {
     int val;
-    ListNode next;
+    listNode next;
     ListNode(int val) {
         this.val = val;
         this.next = null;
@@ -10,10 +10,10 @@ class ListNode {
 }
 
 public class Merge_Two_Sorted_List  {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    public listNode mergeTwoLists(listNode l1, listNode l2) {
         // Create a dummy node to serve as the start of the merged list
-        ListNode dummy = new ListNode(0);
-        ListNode curr = dummy;
+        listNode dummy = new listNode(0);
+        listNode curr = dummy;
 
         // Traverse both lists as long as neither is exhausted
         while (l1 != null && l2 != null) {
@@ -38,26 +38,26 @@ public class Merge_Two_Sorted_List  {
 
     public static void main(String[] args) {
         // Create first linked list: 1 -> 2 -> 4
-        ListNode l1 = new ListNode(1);
-        l1.next = new ListNode(2);
-        l1.next.next = new ListNode(4);
+        listNode l1 = new listNode(1);
+        l1.next = new listNode(2);
+        l1.next.next = new listNode(4);
 
         // Create second linked list: 1 -> 3 -> 4
-        ListNode l2 = new ListNode(1);
-        l2.next = new ListNode(3);
-        l2.next.next = new ListNode(4);
+        listNode l2 = new listNode(1);
+        l2.next = new listNode(3);
+        l2.next.next = new listNode(4);
 
         // Create an instance of Solution and call mergeTwoLists
         Merge_Two_Sorted_List solution = new Merge_Two_Sorted_List();
-        ListNode mergedList = solution.mergeTwoLists(l1, l2);
+        listNode mergedList = solution.mergeTwoLists(l1, l2);
 
         // Print the merged list: 1 -> 1 -> 2 -> 3 -> 4 -> 4
         printList(mergedList);
     }
 
     // Helper method to print a linked list
-    public static void printList(ListNode head) {
-        ListNode current = head;
+    public static void printList(listNode head) {
+        listNode current = head;
         while (current != null) {
             System.out.print(current.val + " ");
             current = current.next;
