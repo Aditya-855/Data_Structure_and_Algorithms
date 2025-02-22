7f196ed7faf33c8bb3794112bd679b69488d811c
Aditya-855 <139979027+Aditya-855@users.noreply.github.com>
2025-02-22 22:33:47 +0530

    Leetcode Question Climbing Stairs

--- src/Arrays/Climbing_Stairs.java
+++ src/Arrays/Climbing_Stairs.java
@@ -4,16 +4,17 @@ package Arrays;
 public class Climbing_Stairs {
     public static void main(String[] args) {
         int n = 3;
+        // Base cases: If there is 1 or 0 steps, there is only one way to climb
+        int prev1 = 1; // Represents ways to reach (n-1)th step
+        int prev2 = 1; // Represents ways to reach (n-2)th step
 
-        int prev2 = 1;
-        int prev = 1;
-
+        // Loop from step 2 to n, since first two cases (n=0,1) are covered by initial values
         for (int i = 2; i <= n; i++) {
-            int cur_i = prev2 + prev;
-            prev2 = prev;
-            prev = cur_i;
-        }
-        System.out.println(prev);
+            int curr_i = prev2 + prev1; // The current step's ways = (ways to (n-1) + ways to (n-2))
+            prev2 = prev1; // Update prev2 to be the old prev1 (moving one step ahead)
+            prev1 = curr_i; // Update prev1 to be the new calculated value
+            }
+        System.out.println(prev1);// Return the final computed number of ways
     }
 }
 
