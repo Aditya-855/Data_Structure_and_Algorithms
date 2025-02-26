a2887d237fd598d319e09f236bb439b70c73ba11
Aditya-855 <139979027+Aditya-855@users.noreply.github.com>
2025-02-26 13:42:09 +0530

    Learned Sorting Algorithm Selection Sort

--- src/Selection_Sort/Selection_Sort.java
+++ src/Selection_Sort/Selection_Sort.java
@@ -1,35 +1,36 @@
 package Selection_Sort;
 
-import java.util.Arrays;
-
 public class Selection_Sort {
-    public static void main(String[] args) {
-        int[] arr={4,5,1,2,3};
-        selectionSort(arr);
-        System.out.println(Arrays.toString(arr));
-    }
-    static void selectionSort(int[] arr){
-        for(int i=0;i<arr.length;i++){
-            //find the max item in the remaining array and swap with correct index
-            int last=arr.length-i-1;
-            int maxIndex=getMaxIndex(arr,0,last);
-
-            swap(arr,maxIndex,last);
-        }
-    }
-
-    static void swap(int[] arr,int first,int second){
-        int temp=arr[first];
-        arr[first]=arr[second];
-        arr[second]=temp;
-    }
-    private static int getMaxIndex(int[] arr,int start,int end){
-        int max=start;
-        for (int i=start;i<=end;i++){
-            if (arr[max]<arr[i]){
-                max=i;
+    static void selection_sort(int arr[], int n) {
+        for (int i = 0; i < n - 1; i++) {
+            int mini = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[mini]) {
+                    mini = j;
+                }
             }
+            //swap
+            int temp = arr[mini];
+            arr[mini] = arr[i];
+            arr[i] = temp;
         }
-        return max;
+
+        System.out.println("After selection sort:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+
+        int arr[] = {13, 46, 24, 52, 20, 9};
+        int n = arr.length;
+        System.out.println("Before selection sort:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        selection_sort(arr, n);
     }
 }
