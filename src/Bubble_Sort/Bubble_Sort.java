17f6896e155910b6af808122c8819e2915d1f18d
Aditya-855 <139979027+Aditya-855@users.noreply.github.com>
2025-02-26 13:49:11 +0530

    Learned Sorting Algorithm Bubble Sort

--- src/Bubble_Sort/Bubble_Sort.java
+++ src/Bubble_Sort/Bubble_Sort.java
@@ -1,34 +1,30 @@
 package Bubble_Sort;
 
-import java.util.Arrays;
-
 public class Bubble_Sort {
-    public static void main(String[] args) {
-        int[] arr={1,2,3,4,5};
-        bubbleSort(arr);
-        System.out.println(Arrays.toString(arr));
-
-    }
-    static void bubbleSort(int[] arr){
-        boolean swapped;
-        //run the steps n-1 times
-        for(int i=0;i<arr.length;i++){
-            swapped=false;
-            //for each step, max item will come at the last respective index
-            for (int j=1;j< arr.length-i;j++){
-                //swap if the item is smaller than the previous item
-                if (arr[j]<arr[j-1]){
-                    //swap
-                    int temp=arr[j];
-                    arr[j]=arr[j-1];
-                    arr[j-1]=temp;
-                    swapped=true;
+    static void bubble_sort(int[] arr, int n) {
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j <= i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
-            //if you did not swap for a particular value of i,it means array is sorted hence stop the program
-            if (!swapped){
-                break;
-            }
         }
+        System.out.println("After bubble sort: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String args[]) {
+        int arr[] = {13, 46, 24, 52, 20, 9};
+        int n = arr.length;
+        System.out.println("Before Using Bubble Sort: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        bubble_sort(arr, n);
     }
 }
