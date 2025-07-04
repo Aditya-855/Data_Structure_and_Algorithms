ea4ccd0daa9decf376e8557e0e0698ad2a14db76
Aditya-855 <adityapanwar855@gmail.com>
2025-07-04 12:44:19 +0530

    Training With Hitbulls Eye For the Betterment in Logic Building and Ehnacement in My Skills

--- src/Dynamic_Programming/Count_Coins.java
+++ src/Dynamic_Programming/Count_Coins.java
@@ -1,35 +1,38 @@
 package Dynamic_Programming;
 
+import java.util.Arrays;
+
 public class Count_Coins {
-    static int count(int[] coins, int sum) {
-        int n = coins.length;
-        // 2d dp array where n is the number of coin
-        // denominations and sum is the target sum
-        int[][] dp = new int[n + 1][sum + 1];
-        // Represents the base case where the target sum is 0,
-        // and there is only one way to make change: by not
-        // selecting any coin
-        dp[0][0] = 1;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= sum; j++) {
+    static int minCoins(int[] coins, int sum)
+    {
+        int[][] dp = new int[coins.length][sum + 1];
 
-                // Add the number of ways to make change without
-                // using the current coin
-                dp[i][j] += dp[i - 1][j];
-
-                if ((j - coins[i - 1]) >= 0) {
-
-                    // Add the number of ways to make change
-                    // using the current coin
-                    dp[i][j] += dp[i][j - coins[i - 1]];
+        for (int i = coins.length - 1; i >= 0; i--)
+        {
+            for (int j = 1; j <= sum; j++)
+            {
+                dp[i][j] = Integer.MAX_VALUE;
+                int take = Integer.MAX_VALUE, noTake = Integer.MAX_VALUE;
+                // If we take coins[i] coin
+                if (j - coins[i] >= 0)
+                {
+                    take = dp[i][j - coins[i]];
+                    if (take != Integer.MAX_VALUE) take++;
                 }
+                // not take the coin
+                if (i + 1 < coins.length) noTake = dp[i + 1][j];
+
+                dp[i][j] = Math.min(take, noTake);
             }
         }
-        return dp[n][sum];
+
+        if (dp[0][sum] != Integer.MAX_VALUE) return dp[0][sum];
+        return -1;
     }
+
     public static void main(String[] args) {
         int[] coins = {1,3,4,5};
         int sum = 7;
-        System.out.println(count(coins, sum));
+        System.out.println(minCoins(coins, sum));
     }
 }
